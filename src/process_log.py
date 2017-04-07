30d5a2355cdc81fd9d5aaefb9be1e3cbb2ab52ba
Shahzad Anwar <sa3341@columbia.edu>
2017-04-07 14:54:39 -0400

    Debug Part 2

--- src/process_log.py
+++ src/process_log.py
@@ -5,15 +5,7 @@ import shlex
 
 #['221.195.71.14', '-', '-', '[01/Jul/1995:00:00:54', '-0400]', '"GET', '/shuttle/missions/sts-71/mission-sts-71.html', 'HTTP/1.0"', '200', '13387\n']
 
-
-#print len(sys.argv)
-#print str(sys.argv)
-
-#print str(sys.argv[1])
-
 log = open(sys.argv[1],"r")
-#print log.read()
-
 
 ##### get top 10 hosts ####
 hostTable = {}
@@ -21,10 +13,13 @@ resourceTable = {}
 count = 0
 for line in log:
     lineList = shlex.split(line, posix=False)
-    resourceName = lineList[5].split(' ')[1]
+    try:
+        resourceName = lineList[5].split(' ')[1]
+    except IndexError:
+        print lineList
     resourceSize = 0 if lineList[-1] == '-' else lineList[-1]
-    if isinstance(resourceSize, str):
-        resourceSize = int(resourceSize)
+    #if isinstance(resourceSize, str):
+    resourceSize = int(resourceSize)
     if resourceName in resourceTable:
         resourceTable[resourceName] += resourceSize
     else:
@@ -34,6 +29,9 @@ for line in log:
         hostTable[host] += 1
     else:
         hostTable[host] = 1
+    if count % 100000 == 0:
+        print count / 100000
+    count += 1
     
 host_file = open(sys.argv[2],"w")
 host_file.writelines([host + "," + str(hostTable[host]) + "\n" for host in sorted(hostTable, key = hostTable.get, reverse = True)[:10]])
@@ -41,17 +39,9 @@ host_file.close()
 
 resource_file = open(sys.argv[4],"w")
 resource_file.writelines([resourceName + "\n" for resourceName in sorted(resourceTable, key = resourceTable.get, reverse = True)[:10]])
-#print [resourceName + "\n" for resourceName in sorted(resourceTable, key = resourceTable.get, reverse = True)[:10]]
-
-
 resource_file.close()
 
 resource_file = open(sys.argv[4],"r")
-print resource_file.read()
-
-
-
-
-
+#print resource_file.read()
 
 log.close()
